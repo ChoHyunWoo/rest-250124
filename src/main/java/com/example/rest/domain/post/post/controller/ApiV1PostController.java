45590757fbf88b9215ab9de1c40fb2536455dc9f
ChoHyunWoo <gusdndlek12@naver.com>
2025-01-27 10:22:39 +0900

    validation apply

--- src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/example/rest/domain/post/post/controller/ApiV1PostController.java
@@ -3,14 +3,13 @@ package com.example.rest.domain.post.post.controller;
 import com.example.rest.domain.post.post.entity.Post;
 import com.example.rest.domain.post.post.service.PostService;
 import com.example.rest.global.dto.RsData;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -24,31 +23,48 @@ public class ApiV1PostController {
         return postService.getItems();
     }
 
+
+    @GetMapping("{id}")
+    public Post getItem(@PathVariable long id) {
+        return postService.getItem(id).get();
+    }
+
+
     @DeleteMapping("/{id}")
     public RsData delete(@PathVariable long id) {
         Post post = postService.getItem(id).get();
         postService.delete(post);
 
-        return new RsData("200-1",
-                "%d번 글 수정이 완료되었습니다.".formatted(id));
+        return new RsData(
+                "200-1",
+                "%d번 글 삭제가 완료되었습니다.".formatted(id)
+        );
     }
 
-
-    @AllArgsConstructor
-    @Getter
-    public static class ModifyForm {
-        private String title;
-        private String content;
-    }
+    record ModifyReqBody(@NotBlank @Length(min=3) String title, @NotBlank @Length(min=3) String content) {}
 
     @PutMapping("{id}")
-    public RsData modify(@PathVariable long id, ModifyForm form) {
+    public RsData modify(@PathVariable long id, @RequestBody @Valid ModifyReqBody body) {
 
         Post post = postService.getItem(id).get();
-        postService.modify(post, form.getTitle(), form.getContent());
+        postService.modify(post, body.title(), body.content());
 
-        return new RsData("200-1",
-                "%d번 글 수정이 완료되었습니다.".formatted(id));
+        return new RsData(
+                "200-1",
+                "%d번 글 수정이 완료되었습니다.".formatted(id)
+        );
+    }
+
+    record WriteReqBody(@NotBlank @Length(min=3) String title, @NotBlank @Length(min=3) String content) {}
+
+    @PostMapping
+    public RsData write(@RequestBody @Valid WriteReqBody body) {
+        postService.write(body.title(), body.content());
+
+        return new RsData(
+                "200-1",
+                "글 작성이 완료되었습니다."
+        );
     }
 
 }
