907d6a552c62d6ec3b3930a7186430329f9a4dc5
ChoHyunWoo <gusdndlek12@naver.com>
2025-01-31 14:17:59 +0900

    message modify

---
+++ src/main/java/com/example/rest/global/app/AppConfig.java
@@ -0,0 +1,13 @@
+package com.example.rest.global.app;
+
+
+
+public class AppConfig {
+
+    public static boolean isNotProd() {
+
+        return true;
+
+    }
+
+}
--- src/main/java/com/example/rest/global/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/rest/global/exception/GlobalExceptionHandler.java
@@ -1,43 +1,67 @@
 package com.example.rest.global.exception;
 
+import com.example.rest.global.app.AppConfig;
 import com.example.rest.global.dto.RsData;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NoSuchElementException.class)
-    public ResponseEntity<RsData<Void>> handle() {
+    public ResponseEntity<RsData<Void>> handle(NoSuchElementException e) {
+
+        // 개발 모드에서만 작동되도록.
+        if(AppConfig.isNotProd()) e.printStackTrace();
 
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
-                .body(new RsData<>(
+                .body(
+                        new RsData<>(
                                 "404-1",
                                 "해당 데이터가 존재하지 않습니다"
                         )
-
                 );
-
-    }
-@ExceptionHandler(MethodArgumentNotValidException.class)
-public ResponseEntity<RsData<Void>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
-    String field = e.getBindingResult().getFieldError().getField();
-    String message = e.getBindingResult().getFieldError().getDefaultMessage();
-    return ResponseEntity
-            .status(HttpStatus.BAD_REQUEST)
-            .body(
-                    new RsData<>(
-                            "400-1",
-                            field + " : " + message
-                    )
-            );
     }
 
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<RsData<Void>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+
+        String message = e.getBindingResult().getFieldErrors()
+                .stream()
+                .map(fe -> fe.getField() + " : " + fe.getCode() + " : "  + fe.getDefaultMessage())
+                .sorted()
+                .collect(Collectors.joining("\n"));
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(
+                        new RsData<>(
+                                "400-1",
+                                message
+                        )
+                );
+    }
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<RsData<Void>> DataIntegrityViolationExceptionHandle(DataIntegrityViolationException e) {
+        // 개발 모드에서만 작동되도록.
+        if(AppConfig.isNotProd()) e.printStackTrace();
+
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(
+                        new RsData<>(
+                                "409-1",
+                                "이미 존재하는 데이터입니다."
+                        )
+                );
+    }
 }
